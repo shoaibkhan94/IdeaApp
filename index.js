d6c622d72906d78f3288abfa2fc69b9b28787b3d
shoaibkhan94 <shoaibkhan1994@gmail.com>
2018-01-16 17:17:50 +0530

    Moved user apis to userController

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const _ = require('lodash');
 const {mongoose} = require('./db/mongoose');
 const {Idea} = require('./models/ideaModel');
 const {User} = require('./models/userModel');
-const {authenticate} = require('./middleware/authentication');
+const userController = require('./controllers/userController');
 
 const PORT = process.env.PORT || 3000;
 
@@ -20,51 +20,7 @@ app.get('/', (req, res) => {
     res.send("IDEA APP");
 });
 
-
-//Create new user by email and password
-app.post('/user', (req, res) => {
-    var body = _.pick(req.body, ['email', 'password']);
-    var newUser = new User(body);
-
-    newUser.save().then(() => {
-        return newUser.generateAuthToken();
-    })
-        .then(token => res.header('x-auth', token).send(newUser))
-        .catch((err) => res.status(400).send(err));
-
-});
-
-
-/*
-*   Login a User
-*   If Email and Password combo is correct send 200 with x-auth header containing jwt token
-*   If Email not found in DB return {message: "Email Not Found"} with 401 status
-*   If Password incorrect return {message: "Incorrect Password"} with 401 status
-*/
-app.post('/user/login', (req, res) => {
-    var body = _.pick(req.body, ['email', 'password']);
-
-    User.findByCredentials(body.email, body.password)
-        .then(user => {
-            return user.generateAuthToken()
-                .then(token => {
-                    res.header('x-auth', token).send(user);
-                });
-        })
-        .catch((err) => res.status(401).send(err));
-});
-
-
-/*
-* Delete token from DB if token is valid
-* */
-app.delete('/user/logout',authenticate, (req, res) => {
-
-    //Got req.user from authenticate middleware which checks for token validity
-    req.user.removeToken().then(() => {
-        res.status(200).send({message: 'Token removed successfully' });
-    }, () => res.status(400).send())
-});
+userController(app);
 
 app.listen(PORT, () => {
     console.log("Server Listening on port ", PORT);
